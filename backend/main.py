11cf6fb37199dd0d7b884e997bfddb462bf369ef
Viewtiful <viewtiful@mail.ru>
2016-10-13 20:59:34 +0300

    first commit

--- backend/main.py
+++ backend/main.py
@@ -7,15 +7,35 @@ except ImportError:
     def getuid():
         return 4000
 
-from flask import Flask
+from flask import Flask, request, render_template
+from pprint import pformat
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello from Flask!"
+@app.route("/index.html")
+def index():
+    return render_template(
+		"index.html",
+		username=request.args.get('name', 'Anonymous')
+		
+	)
+	
+@app.route("/test/<int:first>")
+@app.route("/test/<int:first>/<int:second>")
+def divides(first, second=3):
+	second = int(request.args.get('second', 0))
+	return "Yes" if number % second == 0 else "Now"
+
+@app.route("/test/<anyhing>")
+def anything_str(anything):	
+	return anything
+@app.route("/hello/")
+@app.route("/hello/<username>")
+def hello(username='Anonymus'):
+	return "HELLO, {}".format(username)
 
 
 if __name__ == "__main__":
-    app.run(port=getuid() + 1000)
+    app.run(port=getuid() + 1000, debug = True)
